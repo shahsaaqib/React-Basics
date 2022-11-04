829a176ec0df7c1e2b2baec5c44b5a54cfddd856
SAAQIB ASHRAF <shahsaaqib01@gmail.com>
2022-11-04 09:51:11 +0530

    modification to package.json

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ const App = () => {
   return (
     <div>
       <h1> Adopt Me! </h1>
-      <Pet name = "Luna" animal = "Dog" breed = "Havanese" />
-      <Pet name = "Pepper" animal = "Bird" breed = "Cockatiel" />
-      <Pet name = "Doink" animal = "Cat" breed = "Mix" />
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mix" />
     </div>
-  )
-}
+  );
+};
 
 render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,14 +1,13 @@
 import React from "react";
 
-
 const Pet = (props) => {
-    return (
-        <div>
-            <h2>{ props.name }</h2>
-            <h1>{ props.animal }</h1>
-            <h1>{ props.breed }</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h1>{props.animal}</h1>
+      <h1>{props.breed}</h1>
+    </div>
+  );
+};
 
 export default Pet;
